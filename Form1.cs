ddb4754e7b7c867ad0700faf2900011d7b94e03a
asudekayim <115153313+asudekayim@users.noreply.github.com>
2023-12-07 00:31:08 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -9,23 +9,23 @@ namespace Gelecegi_Yazanlar_1
 
         private void button1_KonumSec_Click(object sender, EventArgs e)
         {
-            //konum seçmek için kullanılır
-            //tek başına kullanılması bir şey ifade etmez birlikte kullanılması gerekir
+            //konum seÃ§mek iÃ§in kullanÃ½lÃ½r
+            //tek baÃ¾Ã½na kullanÃ½lmasÃ½ bir Ã¾ey ifade etmez birlikte kullanÃ½lmasÃ½ gerekir
             folderBrowserDialog1.ShowDialog();
             label1.Text = folderBrowserDialog1.SelectedPath;
         }
 
         private void button_DosyaAc_Click(object sender, EventArgs e)
         {
-            //folder browser dan farkı sadece klasörleri değil dosyaları da gösterir
-            //bazı filtreleme yöntemleriyle sadece istenen türde belge gösterilebilir
+            //folder browser dan farkÃ½ sadece klasÃ¶rleri deÃ°il dosyalarÃ½ da gÃ¶sterir
+            //bazÃ½ filtreleme yÃ¶ntemleriyle sadece istenen tÃ¼rde belge gÃ¶sterilebilir
             openFileDialog1.ShowDialog();
             label1.Text = openFileDialog1.FileName;
         }
 
         private void button_DosyaOlustur_Click(object sender, EventArgs e)
         {
-            //form üzerinden herhangi bir dosya türüne kayıt yapar
+            //form Ã¼zerinden herhangi bir dosya tÃ¼rÃ¼ne kayÃ½t yapar
             saveFileDialog1.ShowDialog();
         }
         string belgeyolu, belgeadi;
@@ -42,7 +42,7 @@ namespace Gelecegi_Yazanlar_1
         {
             belgeadi = textBox2_Ad.Text;
             StreamWriter sw = File.CreateText(belgeyolu + "\\" + belgeadi + ".txt");
-            MessageBox.Show("Belgeniz başarıyla oluşturuldu.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("Belgeniz basariyla olusturuldu.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void button_oku_Click(object sender, EventArgs e)
@@ -63,12 +63,12 @@ namespace Gelecegi_Yazanlar_1
 
         private void button_yazdir_Click(object sender, EventArgs e)
         {
-            saveFileDialog1.Filter = "Metin Dosyası | *.txt";
+            saveFileDialog1.Filter = "Metin DosyasÃ½ | *.txt";
             saveFileDialog1.ShowDialog();
             StreamWriter kaydet = new StreamWriter(saveFileDialog1.FileName);
             kaydet.WriteLine(richTextBox1.Text);
             kaydet.Close();
-            MessageBox.Show("Metin belgesine kayıt yapıldı.", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            MessageBox.Show("Metin belgesine kayÃ½t yapÃ½ldÃ½.", "BÃLGÃ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
         }
     }
 }
